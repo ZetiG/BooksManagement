9b7071e88f1628c93c41d3148020ecc939097131
zhangmk <zmk95511@163.com>
2019-03-30 23:50:27 +0800

    学生列表展示数据库数据

--- BooksManagement/src/com/manage/teacher/MyStudentUI.java
+++ BooksManagement/src/com/manage/teacher/MyStudentUI.java
@@ -13,11 +13,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MyStudentUI extends JFrame {
 
-    private JTextField textField;
+    private JTextField stuName = new JTextField();
+    private JTextField stuNo = new JTextField();
 
     private Connection conn;
     private ResultSet r;
@@ -36,7 +38,7 @@ public class MyStudentUI extends JFrame {
      */
     public MyStudentUI() {
         selectClass();
-        selectAllStu();
+        selectAllStu(null);
 
         setTitle("我的学生");
         setVisible(true);
@@ -48,32 +50,42 @@ public class MyStudentUI extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-        panel.setBounds(25, 30, 385, 73);
+        panel.setBounds(25, 30, 760, 73);
         getContentPane().add(panel);
 
-        JLabel label = new JLabel("学生:");
+        JLabel label = new JLabel("姓名/学号:");
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setFont(new Font("微软雅黑", Font.PLAIN, 12));
         label.setBounds(10, 27, 95, 15);
         panel.add(label);
 
-        textField = new JTextField();
-        textField.setColumns(10);
-        textField.setBounds(101, 24, 125, 21);
-        panel.add(textField);
 
-        JButton button = new JButton("查询现有");
+        stuName.setColumns(10);
+        stuName.setBounds(101, 24, 125, 21);
+        panel.add(stuName);
+
+        JButton button = new JButton("查询学生");
         button.setIcon(new ImageIcon(com.manage.Windos.Book_require.class.getResource("/images/search.png")));
-        button.setBounds(255, 10, 107, 23);
+        button.setBounds(255, 21, 107, 26);
         panel.add(button);
 
-        JButton button_3 = new JButton("查询全部");
-        button_3.setIcon(new ImageIcon(com.manage.Windos.Book_require.class.getResource("/images/search.png")));
-        button_3.setBounds(255, 40, 107, 23);
+        JLabel label2 = new JLabel("学号:");
+        label2.setHorizontalAlignment(SwingConstants.CENTER);
+        label2.setFont(new Font("微软雅黑", Font.PLAIN, 12));
+        label2.setBounds(360, 27, 95, 15);
+        panel.add(label2);
+
+        stuNo.setColumns(10);
+        stuNo.setBounds(450, 24, 125, 21);
+        panel.add(stuNo);
+
+        JButton button_3 = new JButton("删除学生");
+        button_3.setIcon(new ImageIcon(com.manage.Windos.Book_require.class.getResource("/images/delete.png")));
+        button_3.setBounds(600, 21, 107, 26);
         panel.add(button_3);
 
 
-        JLabel label_2 = new JLabel("输入姓名或学号查询");
+        JLabel label_2 = new JLabel("输入姓名或学号查询/删除");
         label_2.setFont(new Font("微软雅黑", Font.PLAIN, 12));
         label_2.setBounds(25, 10, 143, 15);
         getContentPane().add(label_2);
@@ -125,44 +137,67 @@ public class MyStudentUI extends JFrame {
         getContentPane().add(scroll);
 
 
-/*
         button.addActionListener(e -> {
-            if (null != selectClass(sql)) {
-                if (selectAllStu(sql2)) {
-                    JOptionPane.showMessageDialog(null, "查询成功");
+            String stu = stuName.getText();
+            if (null != stu && !stu.isEmpty()) {
+                list.clear();
+                List<Student> studentList = selectAllStu(stu);
+                if (studentList.size() > 0) {
+                    // TODO: 2019-03-30
+                    //final DefaultTableModel dtm = new DefaultTableModel();
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "查不到该学生信息！");
                 }
             }
-            JOptionPane.showMessageDialog(null, "查不到您所代理的班级,请联系管理员！");
         });
-*/
+
+        button_3.addActionListener(e -> {
+            if (null != stuName.getText() && !stuName.getText().isEmpty()) {
+                System.err.println(stuNo.getText());
+            }
+        });
+
     }
 
 
-    public Boolean selectAllStu() {
+    public List<Student> selectAllStu(String student) {
         try {
             conn = OperationSQL.getCon();  //建立数据库连接
             String sql2 = "select stu.stu_no,stu.name,mj.major,stu.sex,stu.age,stu.phone " +
                     "from t_student stu join t_major mj on stu.major=mj.id where stu.is_deleted=0 and stu.major=?";
+            if (null != student) {
+                sql2 = sql2 + " and (stu.name=? or stu_no=?)";
+            }
             PreparedStatement stmt = conn.prepareStatement(sql2);   //会抛出异常
             stmt.setString(1, classNo);
-            r = stmt.executeQuery();
-            while (r.next()) {
-                Student stu = new Student();
-                stu.setStuNo(r.getString(1));
-                stu.setStuName(r.getString(2));
-                stu.setStuMajor(r.getString(3));
-                stu.setStuSex(r.getString(4));
-                stu.setStuAge(r.getString(5));
-                stu.setStuPhone(r.getString(6));
-                list.add(stu);
+            if (null != student) {
+                stmt.setString(2, student);
+                stmt.setString(3, student);
             }
-            return true;
+            System.err.println("执行SQL->:" + sql2);
+            r = stmt.executeQuery();
+            r.last();
+            if (r.getRow() > 0) {
+                r.beforeFirst();
+                while (r.next()) {
+                    Student stu = new Student();
+                    stu.setStuNo(r.getString(1));
+                    stu.setStuName(r.getString(2));
+                    stu.setStuMajor(r.getString(3));
+                    stu.setStuSex(r.getString(4));
+                    stu.setStuAge(r.getString(5));
+                    stu.setStuPhone(r.getString(6));
+                    list.add(stu);
+                }
+                return list;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             OperationSQL.closeConnet();
         }
-        return false;
+        return Collections.EMPTY_LIST;
     }
 
     private String selectClass() {
@@ -171,6 +206,7 @@ public class MyStudentUI extends JFrame {
             String sql = "select class from t_teacher where is_deleted=0 and user_id=?";
             PreparedStatement stmt = conn.prepareStatement(sql);   //会抛出异常
             stmt.setString(1, String.valueOf(Login.userId));
+            System.err.println("执行SQL->:" + sql);
             r = stmt.executeQuery();
             while (r.next()) {
                 classNo = r.getString(1);
